effb96e9b0fba718072d9a680fb91722de30f129
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-06-30 15:39:04 -0400

    🧼

--- javascript-testing-starter/src/intro.js
+++ javascript-testing-starter/src/intro.js
@@ -1,10 +1,7 @@
-// Lesson: Writing your first tests
-// 1.) refactor code
 export function max(a, b) {
   return a > b ? a : b;
 }
 
-// Exercise
 export function fizzBuzz(n) {
   if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
   if (n % 3 === 0) return "Fizz";
@@ -12,11 +9,8 @@ export function fizzBuzz(n) {
   return n.toString();
 }
 
-// 3.) create the calculateAverage()
 export function calculateAverage(numbers) {
-  // 8.) make test pass new test case
   if (numbers.length === 0) return NaN;
   return numbers[0];
-  // 5.) make test pass with the simplest implementation
   return NaN;
 }
