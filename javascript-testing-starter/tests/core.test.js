35d42147c051d3366cea6cbe4be65ff52ccab3a5
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-07-01 12:58:55 -0400

    🧠 Make assertions on an object

--- javascript-testing-starter/tests/core.test.js
+++ javascript-testing-starter/tests/core.test.js
@@ -1,5 +1,15 @@
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 
 describe("test suite", () => {
-  it("test case", () => {});
+  it("test case", () => {
+    const result = { name: "Craig" };
+    // Make a tight assertion - If you add a prop this will break
+    expect(result).toEqual({ name: "Craig" });
+    // Make more general assertion - Check for the presence of a subset of properties in this object
+    expect(result).toMatchObject({ name: "Craig" });
+    // Optional assertion - Check for the presence of the name prop. Who cares about the actual name.
+    expect(result).toHaveProperty("name");
+    // Make assertion for it to be a string
+    expect(typeof result.name).toBe("string");
+  });
 });
