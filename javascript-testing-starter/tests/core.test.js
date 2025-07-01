93091618c5fa0e29dc8e31e3c95e4f93b81b546b
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-07-01 12:22:21 -0400

    🧼

--- javascript-testing-starter/tests/core.test.js
+++ javascript-testing-starter/tests/core.test.js
@@ -1,15 +1,5 @@
-import { describe, expect, it } from "vitest";
+import { describe, it } from "vitest";
 
 describe("test suite", () => {
-  it("test case", () => {
-    // 1.) Capture the results from the functions we want to test
-    const result = "The requested file was not found";
-
-    // 2.) Make a loose assertion - No matter what the result is this test will pass.
-    expect(result).toBeDefined();
-    // 3.) Make a tight assertion - One little change to production code can break this.
-    expect(result).toBe("The requested file was not found");
-    // 4.) Make better assertion - In this case you would use a keyword or phase
-    expect(result).toMatch(/not found/i);
-  });
+  it("test case", () => {});
 });
