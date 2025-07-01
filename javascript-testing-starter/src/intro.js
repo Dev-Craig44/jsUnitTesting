59b6523d97037ec0572cbe15886043b937a264f6
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-07-01 01:24:55 -0400

    🧠 Write test suite for factorial()

--- javascript-testing-starter/src/intro.js
+++ javascript-testing-starter/src/intro.js
@@ -16,3 +16,13 @@ export function calculateAverage(numbers) {
   });
   return sum / numbers.length;
 }
+
+// 0! = 1
+// 1! = 1
+// 2! = 2 x 1
+// 3! = 3 x 2 x 1
+
+export function factorial(n) {
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
+}
