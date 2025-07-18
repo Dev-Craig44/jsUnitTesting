ae93bddca0dd0b3308a724882428f0c35494600f
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-07-18 00:59:47 -0400

    🧼 Refactor: make the mock function return a promnise

--- javascript-testing-starter/tests/mocking.test.js
+++ javascript-testing-starter/tests/mocking.test.js
@@ -1,7 +1,6 @@
 // 1.) download essential function with `iv`
 // 2.) also download the `vi` object from vitest
-
-const { describe, vi } = require("vitest");
+import { describe, it, vi } from "vitest";
 
 // 3.) Create a test suite
 describe("test suite", () => {
@@ -11,11 +10,12 @@ describe("test suite", () => {
     const greet = vi.fn();
     // Off rip, this is an empty function, but if we program it, before we call it, and make it return a value
     // 6.) give greet() a return value using the {mockReturnValue} method
-    greet.mockReturnValue("Hello");
+    greet.mockResolvedValue("Hello");
 
-    // 7.) Call the function and store the results in [result]
-    const result = greet();
-    // 8.) Log the results
-    console.log(result);
+    // 7.) Call the function, which returns a promise, use the {then} method to handle the resolved value
+    greet().then((result) => {
+      // 8.) Log the result to the console
+      console.log(result);
+    });
   });
 });
