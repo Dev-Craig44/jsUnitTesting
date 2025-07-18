c78aa123a88756ca7407271629ce68db3ad3b9fa
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-07-18 01:24:00 -0400

    🧠 Make assertion for our mock function to be called only once

--- javascript-testing-starter/tests/mocking.test.js
+++ javascript-testing-starter/tests/mocking.test.js
@@ -13,7 +13,7 @@ describe("test suite", () => {
 
     // 7.) Call the function, and pass it a name, store it in [result]
     const result = greet("Craig");
-    // 8.) Make assertion for this function to be called with `Craig`
-    expect(greet).toHaveBeenCalledWith("Craig");
+    // 8.) Make assertion for this function to be called only once
+    expect(greet).toHaveBeenCalledOnce();
   });
 });
