fd674a5ba536ac7b05faf46d9575a88ce2a1f0ac
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-07-18 01:21:39 -0400

    🧠 Make assertion for our mock function to be called with a certain value

--- javascript-testing-starter/tests/mocking.test.js
+++ javascript-testing-starter/tests/mocking.test.js
@@ -1,6 +1,6 @@
 // 1.) download essential function with `iv`
 // 2.) also download the `vi` object from vitest
-import { describe, it, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 
 // 3.) Create a test suite
 describe("test suite", () => {
@@ -13,7 +13,7 @@ describe("test suite", () => {
 
     // 7.) Call the function, and pass it a name, store it in [result]
     const result = greet("Craig");
-    // 8.) Make assertion for this function to be called
-    expect(greet).toHaveBeenCalled();
+    // 8.) Make assertion for this function to be called with `Craig`
+    expect(greet).toHaveBeenCalledWith("Craig");
   });
 });
