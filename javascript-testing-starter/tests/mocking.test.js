eca3fb388b8bcff9b92530fe2f1af4c491bb6c72
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-07-23 08:32:05 -0400

    🧼 Refactor: Correct sendMessage() implementation

--- javascript-testing-starter/tests/mocking.test.js
+++ javascript-testing-starter/tests/mocking.test.js
@@ -2,13 +2,16 @@ import { describe, expect, it, vi } from "vitest";
 
 describe("test suite", () => {
   it("test case", () => {
-    const sendMessage = vi.fn();
+    const sendText = vi.fn();
+    // 1.) Program this to return a particular result
+    sendText.mockReturnValue("ok");
+    // 2.) Call this function
+    const result = sendText("message");
 
-    sendMessage.getMockImplementation((message) => console.log(message));
+    // 3.) Assert that the function is called w/ the message
+    expect(sendText).toHaveBeenCalledWith("message");
 
-    const result = sendMessage("Yo wass good?");
-
-    expect(sendMessage).toHaveBeenCalled();
-    expect(sendMessage).toHaveBeenCalledWith("Yo wass good?");
+    // 4.) Assert that the result is 'ok'
+    expect(result).toBe("ok");
   });
 });
