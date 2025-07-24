6487974c405f6aeab18c7bca5a728b6338f27733
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-07-24 01:22:46 -0400

    🧠 Exercise: Testing getShippingInfo()

--- javascript-testing-starter/tests/mocking.test.js
+++ javascript-testing-starter/tests/mocking.test.js
@@ -1,8 +1,11 @@
 import { describe, expect, it, vi } from "vitest";
 import { getExchangeRate } from "../src/libs/currency";
-import { getPriceInCurrency } from "../src/mocking";
+import { getShippingQuote } from "../src/libs/shipping";
+import { getPriceInCurrency, getShippingInfo } from "../src/mocking";
 
 vi.mock("../src/libs/currency");
+// 4.) Mock the shippingQuote module
+vi.mock("../src/libs/shipping");
 
 describe("test suite", () => {
   it("test case", () => {
@@ -23,5 +26,23 @@ describe("getPriceInCurrency", () => {
     const price = getPriceInCurrency(10, "AUD");
 
     expect(price).toBe(15);
+  });
+});
+
+// 1.) Create a test for getShippingInfo()
+describe("getShippingInfo", () => {
+  // 2.) Create a test case for it to return the shipping cost and ETA
+  it("should return the shipping cost and the ETA", () => {
+    // 5.) Call the mocked getShippingQuote function
+    vi.mocked(getShippingQuote).mockReturnValue({
+      cost: 40,
+      estimatedDays: 5,
+    });
+
+    // 3.) Call the functions and store result in [quote]
+    const quote = getShippingInfo("New Jersey");
+
+    // 6.) make assertion that the quote is correct
+    expect(quote).toMatch("/shipping cost 40 days 5/i");
   });
 });
