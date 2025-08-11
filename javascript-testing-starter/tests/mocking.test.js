d0437607c2cfad76aaf34fc19f508e32a1426e85
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-08-11 12:45:18 -0400

    🧼

--- javascript-testing-starter/tests/mocking.test.js
+++ javascript-testing-starter/tests/mocking.test.js
@@ -10,7 +10,6 @@ import {
 
 vi.mock("../src/libs/currency");
 vi.mock("../src/libs/shipping");
-// 8.) Mock the anylytics functions
 vi.mock("../src/libs/analytics");
 
 describe("test suite", () => {
@@ -55,24 +54,16 @@ describe("getShippingInfo", () => {
   });
 });
 
-// 1.) Create a test suite for renderPage()
 describe("renderPage", () => {
-  // 2.) Make 1st test case to return the right content
   it("should return correct content", async () => {
-    // 3.) Call the function and make sure you await it because it returns a promise.
     const result = await renderPage();
 
-    // 4.) Make assertion that the content. We don't want to match the full markup, because the markup can change in the future. So look for special key words.
     expect(result).toMatch(/content/i);
   });
 
-  // 5.) Make 2nd test case that it should call the analytics
   it("should call analytics", async () => {
-    // 6.) call the function
     await renderPage();
 
-    // 7.) Make assertion that the analytics function was called. Note where the function that we want to mock is located.
     expect(trackPageView).toHaveBeenLastCalledWith("/home");
-    // 9.) Need the exact argument becasue we want to make sure the correct path is sent
   });
 });
