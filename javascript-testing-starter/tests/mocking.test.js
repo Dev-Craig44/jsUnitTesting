789d8e85880710eca41eda87aade56f7418bd7a0
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-08-15 11:22:14 -0400

    🧼

--- javascript-testing-starter/tests/mocking.test.js
+++ javascript-testing-starter/tests/mocking.test.js
@@ -3,6 +3,7 @@ import { trackPageView } from "../src/libs/analytics";
 import { getExchangeRate } from "../src/libs/currency";
 import { sendEmail } from "../src/libs/email";
 import { charge } from "../src/libs/payment";
+import security from "../src/libs/security";
 import { getShippingQuote } from "../src/libs/shipping";
 import {
   getPriceInCurrency,
@@ -12,8 +13,6 @@ import {
   signUp,
   submitOrder,
 } from "../src/mocking";
-// 4.) Import the security module
-import security from "../src/libs/security";
 
 vi.mock("../src/libs/currency");
 vi.mock("../src/libs/shipping");
@@ -114,17 +113,8 @@ describe("submitOrder", () => {
 describe("signUp", () => {
   const email = "bullshit@gmail.com";
 
-  // 1.) Create a before each hook to clear our mock function before each test
   beforeEach(() => {
-    // 2.) Call vi.mocked() to gain access to all the mock methods, and call mockClear().
-    // This clears the state of our mocked function before each test.
     vi.mocked(sendEmail).mockClear();
-
-    // If we have multiple mock functions in this test suite, we should also clear each of them in the beforeEach or afterEach hook.
-
-    // We could also call vi.clearAllMocks() to clear all mocks, but this could make debugging harder.
-
-    // We also can configure vitest to automatically clear all mocks before running each test. This way we don't have to remember to clear our mocks in each test suite.
   });
 
   it("should return false if email is not valid", async () => {
@@ -143,13 +133,10 @@ describe("signUp", () => {
 
   it("should send the welcome email if email is valid", async () => {
     const result = await signUp(email);
-    // Our mocked functions like sendEmail track their call history and store it in an object that is accessible via this mocked() function
+
     expect(sendEmail).toHaveBeenCalledOnce();
-
-    // We also have two other test cases that call sendEmail(), so if we wanted to verify if sendEmail was called once, it would fail because our mocked function has been called multiple times before this test case.
-
-    // So the {mock} object is global. It accumulates information between different test cases. So as a best practice, we should always clear our mock functions before or after each test.
     const args = vi.mocked(sendEmail).mock.calls[0];
+
     expect(args[0]).toBe(email);
     expect(args[1]).toMatch(/welcome/i);
   });
